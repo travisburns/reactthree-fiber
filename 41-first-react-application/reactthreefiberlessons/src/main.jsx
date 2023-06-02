9f3410741d7eeaeffb85820c5bb59031c8132625
Travis <travisjb0@gmail.com>
2023-06-01 17:00:49 -0700

    Working with basic review files

--- 41-first-react-application/reactthreefiberlessons/src/main.jsx
+++ 41-first-react-application/reactthreefiberlessons/src/main.jsx
@@ -3,8 +3,15 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+const toto = 'there'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+   
+
+   <h1 className='title'>
+    Hello { toto }
+   </h1>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, inventore!</p>
   </React.StrictMode>,
 )
