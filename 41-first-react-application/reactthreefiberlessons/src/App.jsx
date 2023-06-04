0254d8317e729c9d207ffce23211963044082e58
Travis <travisjb0@gmail.com>
2023-06-04 16:42:45 -0700

    Practiced useMemo hook

--- 41-first-react-application/reactthreefiberlessons/src/App.jsx
+++ 41-first-react-application/reactthreefiberlessons/src/App.jsx
@@ -1,9 +1,10 @@
-import { useState } from 'react'
+import { useMemo, useState } from 'react'
 import Clicker from './Clicker'
 
 
 
-export default function App({children}) {
+
+export default function App({clickersCount, children}) {
   
   const [hasClicker, setHasClicker] = useState(true)
   const [click, setClick] = useState(0)
@@ -19,6 +20,23 @@ const increment = () => {
   setCount(count + 1)
 }
   
+
+
+  const colors =  useMemo(()=> {
+    const colors = {}
+
+ for(let i =0; i < clickersCount; i++) 
+  colors.push (`hsl(${ Math.random() * 360 }deg, 100%, 70%)`)
+
+  return colors
+  }, [])
+
+const tempArray = [...Array(clickersCount)]
+
+tempArray.map((value, index)=> {
+
+})
+
   return <>
   {children}
 
@@ -26,9 +44,17 @@ const increment = () => {
 
   <button onClick={ toggleClickerClick }>{ hasClicker ? 'Hide' : 'Show' } clicker </button>
   { hasClicker && <>
-    <Clicker increment={increment} keyName="countA" color={`hsl(${ Math.random() * 360 }deg, 100%, 70%)`} />
-    <Clicker increment={increment} keyName="countB" color={`hsl(${ Math.random() * 360 }deg, 100%, 70%)`}/>
-    <Clicker increment={increment} keyName="countC" color={`hsl(${ Math.random() * 360 }deg, 100%, 70%)`}/>
+    { [...Array(clickersCount)].map((value, index) => 
+    {
+        <Clicker 
+        key={ index }
+        increment={increment} 
+        keyName={`count${index}`} 
+        color={colors [index] } 
+        />
+    }) }
+    {/* <Clicker increment={increment} keyName="countB" color={`hsl(${ Math.random() * 360 }deg, 100%, 70%)`}/>
+    <Clicker increment={increment} keyName="countC" color={`hsl(${ Math.random() * 360 }deg, 100%, 70%)`}/> */}
   </>}
   
   
--- 41-first-react-application/reactthreefiberlessons/src/main.jsx
+++ 41-first-react-application/reactthreefiberlessons/src/main.jsx
@@ -8,7 +8,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-  <App>
+  <App clickersCount ={3}>
   <h1>My First React Application</h1>
   <h2>And a fancy website</h2>
   
