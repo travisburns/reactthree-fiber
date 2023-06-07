e3b5a37bb0c71b240f5fe1c05cbc3764a837172b
Travis <travisjb0@gmail.com>
2023-06-06 18:26:43 -0700

    Added mesh materials and attributes to the sphere and changed it into a cube

--- 41-first-react-application/reactthreefiberlessons/src/Experience.jsx
+++ 41-first-react-application/reactthreefiberlessons/src/Experience.jsx
@@ -5,9 +5,17 @@ export default function Experience()
 {
     return <>
     <Canvas>
-  <mesh>
-    <sphereGeometry />
-    <meshBasicMaterial />
+  <mesh rotation-y={Math.PI * 0.25} position-x={ 2 } scale={1.5}>
+    <boxGeometry scale={1.5}/>
+    <meshBasicMaterial color="mediumpurple" wireframe={false}/>
+  </mesh>
+  <mesh rotation-y={Math.PI * 0.25} position-x={ -3 } scale={1.5}>
+    <sphereGeometry scale={1.5}/>
+    <meshBasicMaterial color="orange" wireframe/>
+  </mesh>
+  <mesh rotation-y={Math.PI * 0.40} position-x={ 1 } scale={1.5}>
+    <planeGeometry scale={1.5}/>
+    <meshBasicMaterial color="green" wireframe={false}/>
   </mesh>
 </Canvas>
     
