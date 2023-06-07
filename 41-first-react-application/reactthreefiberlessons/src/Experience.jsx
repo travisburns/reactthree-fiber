11179065ed0016f0ea833ae9644494331bb89cbb
Travis <travisjb0@gmail.com>
2023-06-07 16:14:09 -0700

    Working with the directionallight of objects in the canvas

--- 41-first-react-application/reactthreefiberlessons/src/Experience.jsx
+++ 41-first-react-application/reactthreefiberlessons/src/Experience.jsx
@@ -1,6 +1,8 @@
 import { Canvas, useFrame } from '@react-three/fiber'
 import {useRef} from 'react'
 
+
+
 export default function Experience() 
 {
  
@@ -14,23 +16,25 @@ export default function Experience()
   })
 
     return <>
- 
+
+    <directionalLight position={[1, 2, 3]} intensity={1.5} />
+  <ambientLight intensity={0.5} />
 <group ref={groupRef}>
  <mesh rotation-y={Math.PI * 0.25} position-x={ -3 } scale={1.5}>
     <sphereGeometry scale={1.5}/>
-    <meshBasicMaterial color="orange" wireframe={false}/>
+    <meshStandardMaterial color="orange" wireframe={false}/>
   </mesh>
 
   <mesh ref={cubeRef} rotation-y={Math.PI * 0.25} position-x={ 2 } scale={1.5}>
     <boxGeometry scale={1.5}/>
-    <meshBasicMaterial color="mediumpurple" wireframe={false}/>
+    <meshStandardMaterial color="mediumpurple" wireframe={false}/>
   </mesh>
 
   
 </group>
 <mesh position-y={ -1} rotation-x={ - Math.PI * 0.5 } scale={10}>
     <planeGeometry scale={1.5}/>
-    <meshBasicMaterial color="green" wireframe={false}/>
+    <meshStandardMaterial color="green" wireframe={false}/>
   </mesh>
     
     </>
